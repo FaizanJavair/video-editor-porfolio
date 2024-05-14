c634a50c15e643eb1249895cc5faa041b8c17a2e
faizanjavair <faizanjavair@gmail.com>
2024-05-14 12:20:40 +0500

    Added new image

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Arshi Fatima',
     title: "Video Editor & Content Creator",
     desciption:"In the quiet of creation, she finds her gaze, Not on the empty sky, but the cloud's deep haze. A palette of greens, unusual and rare, Foretells of what's coming, a story to share. A video sorceress, a master of art, In her hands, stories find their perfect start. ",
-    image: '../assets/png/IMG_5741.JPG',
+    image: '../assets/jpg/IMG_5903.JPG',
     resumePdf: resume
 }
